afcb5cde6e528b0b27cb69c7515f2ed637ac61db
Ivan Sheyenko <ivs@sitecore.net>
2018-05-08 17:42:20 -0400

    repo init

---
+++ src/Sitecore.Support.220466/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.220466")]
+[assembly: AssemblyProduct("Sitecore.Support.220466")]
+[assembly: ComVisible(false)]
